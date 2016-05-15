89e9e1f722d2617cec23b19b0d159d0eca377af7
Will Schaefer III <wschaeferiii@gmail.com>
2016-05-15 13:18:05 -0400

    Added static pages routes with ejs-layouts

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('layout', 'layout');
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
