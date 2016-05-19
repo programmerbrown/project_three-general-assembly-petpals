623afe920d85d12b23bcbf3d4ec531f230cd600c
Will Schaefer III <wschaeferiii@gmail.com>
2016-05-18 23:11:14 -0400

    Probably not the best way to go about making comment, commented out ajax

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,62 +1,61 @@
-console.log("it works!");
+// console.log("it works!");
 
-$(function() {
+// $(function() {
 
-  var newComment = {};
+//   var newComment = {};
 
-  // GET COMMENTS
-  function getComments() {
-    var comments="";
-    $.ajax({
-        url: 'http://localhost:3000/posts',
-        success: function(data) {
-          // console.log(data);
-          comments = data;
-          $.each(comments, function(index, comment) {
-            $('#comments').append("<li>" + comment.commentText + "</em></li>");
-          });
-        }
-    });
-  }
+//   // GET COMMENTS
+//   function getComments() {
+//     var comments="";
+//     $.ajax({
+//         url: 'http://localhost:3000/posts',
+//         success: function(data) {
+//           // console.log(data);
+//           comments = data;
+//           $.each(comments, function(index, comment) {
+//             $('#comments').append("<li>" + comment.commentText + "</em></li>");
+//           });
+//         }
+//     });
+//   }
 
-  getComments();
+//   getComments();
 
-  // GRAB COMMENT DATA
-  $('#new-comment').submit(function(event) {
-    event.preventDefault();
+//   // GRAB COMMENT DATA
+//   $('#new-comment').submit(function(event) {
+//     event.preventDefault();
 
-    var comment = $(this).find('input[name="commentText"]').val();
+//     var commentText = $(this).find('input[name="commentText"]').val();
+//     var petName = $(this).find('select[name="pet"]').val();
 
-    var pet = $(this).find('select[name="pet"]').val();
+//     var newComment = {
+//       "petName": petName,
+//       "commentText": commentText
+//     };
+//     addComment(newComment);
+//     createNewComment(newComment);
+//     });
 
-    var newComment = {
-      "pet": pet,
-      "commentText": comment
-    };
+//   // SAVE COMMENT DATA
+//   function createNewComment(newComment) {
+//     $.ajax({
+//       url: 'http://localhost:3000/posts',
+//       type: 'POST',
+//       dataType: 'json',
+//       data: newComment,
+//       success: function(data){
+//         console.log(data);
+//         addComment(data);
+//       }
+//     });
+//   }
 
-    addComment(newComment);
-    });
+//   // ADD COMMENT TO VIEW
+//   function addComment(comment) {
+//   $('#comments').append("<li>" + comment.petName + " - <em>" + comment.commentText + "</em> </li>");
+//   }
 
-  // SAVE COMMENT DATA
-  function createNewComment(newComment) {
-    $.ajax({
-      url: 'http://localhost:3000/posts',
-      type: 'POST',
-      dataType: 'json',
-      data: newComment,
-      success: function(data){
-        console.log(data);
-        addComment(data);
-      }
-    });
-  }
-
-  // ADD COMMENT TO VIEW
-  function addComment(comment) {
-  $('#comments').append("<li>" + comment.pet + " - <em>" + comment.commentText + "</em> </li>");
-  }
-
-});
+// });
 
 
 
