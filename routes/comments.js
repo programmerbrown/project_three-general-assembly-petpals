5623f1f0e9c5a9b177ddff086b64034498172ddf
Will Schaefer III <wschaeferiii@gmail.com>
2016-05-20 00:29:37 -0400

    trying to save pets to comments

---
+++ routes/comments.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var passport = require('passport');
+var session = require('express-session');
+var Post = require('../models/post');
+var Pet = require('../models/pet');
+var Comment = require('../models/comment');
+var mongoose = require('mongoose');
+
+
+var commentsRouter = express.Router();
+
+function makeError(res, message, status) {
+  res.statusCode = status;
+  var error = new Error(message);
+  error.status = status;
+  return error;
+}
+
+function authenticate(req, res, next) {
+  if (!req.isAuthenticated()) {
+    res.redirect('/');
+  } else {
+    next();
+  }
+}
+
+// CREATE COMMENT
+commentsRouter.post('/posts/:id/comment', function(req, res, next) {
+  var commentPetId = req.body.pet;
+  var commentText = req.body.commentText;
+  var comment = new Comment ({
+    pet: commentPetId,
+    commentText: commentText
+  });
+  return comment.save()
+  .then(function(comment) {
+  Post.findById(req.params.id)
+  })
+  .then(function(post) {
+    post.comments.push(comment);
+    return post.save();
+  })
+  .then(function(savedPost) {
+    res.redirect('/posts');
+  }, function(err) {
+    return next(err);
+  });
+});
+
+
+module.exports = commentsRouter;
